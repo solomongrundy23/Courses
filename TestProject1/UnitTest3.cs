4a2b516f75ac5549a730cbf5eaaf4a41aa9ce736
Solomon Nil <archanger32tmb@gmail.com>
2022-04-10 19:57:54 +0300

    First Autotests

---
+++ TestProject1/UnitTest3.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class UnitTest3
+    {
+        [Test]
+        public void TestPositive()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestNegative()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
